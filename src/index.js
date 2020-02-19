85ed8d9c644ba0dc7b70340e554dac4a8e1b60ef
김영석Yeongseok Kim <yeongseok.kim@bespinglobal.com>
2020-02-19 16:32:50 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var element=React.createElement('h1',{className:'greeting'},'Hello World!')
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(element,document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
